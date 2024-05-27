13e7eb14ca4fa8f6897b54b254bd2a18e7364a63
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-05-27 20:31:46 +0500

    100% Faster Solution

---
+++ 2124. Check if All A's Appears Before All B's.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    bool checkString(string s)
+    {
+
+        for (int i = 1; i < s.length(); i++)
+        {
+            if (s[i - 1] == 'b' && s[i] == 'a')
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
