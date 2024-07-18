868692d982a16a32a59a1ce3e9102cee37ccf17f
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-07-18 16:13:26 +0500

    2 Approaches :)

--- 234. Palindrome Linked List.cpp
+++ 234. Palindrome Linked List.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// Approach 1:
+
 class Solution
 {
 public:
@@ -37,5 +40,70 @@ public:
         }
 
         return true;
+    }
+};
+
+// Approach 2:
+
+class Solution
+{
+
+private:
+    ListNode *GetMiddle(ListNode *Head)
+    {
+        ListNode *Slow = Head;
+        ListNode *Fast = Head;
+
+        while (Fast != NULL && Fast->next != NULL)
+        {
+            Slow = Slow->next;
+            Fast = Fast->next->next;
+        }
+
+        return Slow;
+    }
+
+    ListNode *ReverseLL(ListNode *Head)
+    {
+        ListNode *Current = Head;
+        ListNode *Previous = NULL;
+        ListNode *Forward = NULL;
+
+        while (Current != NULL)
+        {
+            Forward = Current->next;
+            Current->next = Previous;
+            Previous = Current;
+            Current = Forward;
+        }
+
+        return Previous;
+    }
+
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        if(head==NULL || head->next==NULL)
+        return true;
+
+    ListNode* Middle = GetMiddle(head);
+    ListNode* SecondHalfStart = ReverseList(Middle);
+
+    ListNode* Head1 = head;
+    ListNode* Head2 = SecondHalfStart;
+
+    while(Head2!=NULL)
+    {
+        if(Head1->val!=Head2->val)
+        {
+            return false;
+        }
+        Head1 = Head1 ->next;
+        Head2 = Head2 ->next;
+
+        ReverseList(SecondHalfStart);
+
+    }
+    return true;
     }
 };
