95d7ccf550076ab8d2f34ee62a9614d4c40efaa8
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-08-13 14:24:05 +0500

    Accepted Solution

---
+++ 3248. Snake in Matrix.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int finalPositionOfSnake(int n, vector<string> &commands)
+    {
+
+        vector<vector<int>> Grid(n, vector<int>(n));
+        int count = 0;
+
+        for (auto &x : Grid)
+            for (auto &y : x)
+                y = count++;
+
+        int CurrentRow = 0;
+        int CurrentCol = 0;
+
+        for (int i = 0; i < commands.size(); i++)
+        {
+            if (commands[i] == "LEFT")
+                CurrentCol--;
+
+            if (commands[i] == "RIGHT")
+                CurrentCol++;
+
+            if (commands[i] == "UP")
+                CurrentRow--;
+
+            if (commands[i] == "DOWN")
+                CurrentRow++;
+        }
+
+        return Grid[CurrentRow][CurrentCol];
+    }
+};
