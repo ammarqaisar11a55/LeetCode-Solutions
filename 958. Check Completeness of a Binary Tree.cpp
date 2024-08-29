6978b43955a73bfc2fb0b49ee72dc53da4412781
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-08-29 13:11:02 +0500

    2 Approaches

--- 958. Check Completeness of a Binary Tree.cpp
+++ 958. Check Completeness of a Binary Tree.cpp
@@ -44,3 +44,52 @@ public:
         return true;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void DFS(TreeNode *root, int index, int total, int &ans)
+    {
+        if (root == nullptr)
+            return;
+
+        if (index > total)
+        {
+            ans = 0;
+            return;
+        }
+
+        DFS(root->left, 2 * index, total, ans);
+        DFS(root->right, 2 * index + 1, total, ans);
+    }
+
+    int count(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+
+        return 1 + count(root->left) + count(root->right);
+    }
+
+    bool isCompleteTree(TreeNode *root)
+    {
+
+        int total = count(root);
+        int ans = 1;
+
+        DFS(root, 1, total, ans);
+
+        return ans == 1 ? true : false;
+    }
+};
