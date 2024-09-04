e5a4445aa2751df72c9dcabd4d130159aa37bb1d
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-09-04 12:23:51 +0500

    Accepted Solution

---
+++ 766. Toeplitz Matrix.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    bool isToeplitzMatrix(vector<vector<int>> &matrix)
+    {
+
+        for (int i = 1; i < matrix.size(); i++)
+        {
+            for (int j = 1; j < matrix[i].size(); j++)
+            {
+                if (i - 1 > -1 && j - 1 > -1)
+                {
+                    if (matrix[i][j] != matrix[i - 1][j - 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
