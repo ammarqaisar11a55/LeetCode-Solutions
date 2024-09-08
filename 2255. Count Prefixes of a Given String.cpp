f6aa4f6d7f008d0af479119ce9705082a7e36e0a
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-09-08 19:08:06 +0500

    2 Approaches

--- 2255. Count Prefixes of a Given String.cpp
+++ 2255. Count Prefixes of a Given String.cpp
@@ -15,3 +15,21 @@ public:
         return count;
     }
 };
+
+class Solution
+{
+public:
+    int countPrefixes(vector<string> &words, string s)
+    {
+
+        int count = 0;
+
+        for (int i = 0; i < words.size(); i++)
+        {
+            if (words[i] == s.substr(0, words[i].length()))
+                count++;
+        }
+
+        return count;
+    }
+};
