0f54609f5233f31ec407cb959700a22584f55c68
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-09-10 08:16:54 +0500

    2 Approaches

--- 2605. Form Smallest Number From Two Digit Arrays.cpp
+++ 2605. Form Smallest Number From Two Digit Arrays.cpp
@@ -23,3 +23,36 @@ public:
         return min(*min_element(nums1.begin(), nums1.end()) * 10 + *min_element(nums2.begin(), nums2.end()), *min_element(nums2.begin(), nums2.end()) * 10 + *min_element(nums1.begin(), nums1.end()));
     }
 };
+
+class Solution
+{
+public:
+    int minNumber(vector<int> &nums1, vector<int> &nums2)
+    {
+
+        unordered_map<int, int> Mp;
+
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            Mp[nums1[i]]++;
+        }
+
+        for (int i = 0; i < nums2.size(); i++)
+        {
+            Mp[nums2[i]]++;
+        }
+
+        int mini = INT_MAX;
+
+        for (auto x : Mp)
+        {
+            if (x.second == 2)
+                mini = min(mini, x.first);
+        }
+
+        if (mini != INT_MAX)
+            return mini;
+
+        return min(*min_element(nums1.begin(), nums1.end()) * 10 + *min_element(nums2.begin(), nums2.end()), *min_element(nums2.begin(), nums2.end()) * 10 + *min_element(nums1.begin(), nums1.end()));
+    }
+};
