6e9e4f5db1aa6a662025c674565d42b13e3f55cb
ammarqaisar11a55 <ammarqaisar1130@gmail.com>
2024-09-18 12:10:36 +0500

    test for push in terminsl

---
+++ new-git.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cstdlib>
+#include <unordered_map>
+#include <map>
+#include <functional>
+#include <numeric>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
