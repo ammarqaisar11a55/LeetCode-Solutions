c712de3910bb123d34f50940ab155a12971cafba
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2024-12-18 21:31:28 +0500

    2 Approaches

--- 392. Is Subsequence.cpp
+++ 392. Is Subsequence.cpp
@@ -23,3 +23,23 @@ public:
         return sk.empty() == true ? 1 : false;
     }
 };
+
+class Solution
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+
+        int S_Index = 0;
+
+        for (int i = 0; i < t.length(); i++)
+        {
+            if (S_Index < s.length() && s[S_Index] == t[i])
+            {
+                S_Index++;
+            }
+        }
+
+        return S_Index >= s.length() ? true : false;
+    }
+};
