1dfbaa82b9f53aa1597bef17a048898c31ebf78d
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-01-02 17:46:33 +0500

    Accepted Solution

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -10,3 +10,21 @@ public:
         return s == t;
     }
 };
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+
+        unordered_map<char, int> mp1, mp2;
+
+        for (char c : s)
+            mp1[c]++;
+
+        for (char c : t)
+            mp2[c]++;
+
+        return mp1 == mp2 ? true : false;
+    }
+};
