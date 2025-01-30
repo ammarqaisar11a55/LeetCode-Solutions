b685dc6168c0345ebf7552db7fb7504d6422d935
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-01-31 00:24:55 +0500

    2 Approaches

--- 2396. Strictly Palindromic Number.cpp
+++ 2396. Strictly Palindromic Number.cpp
@@ -6,3 +6,43 @@ public:
         return false;
     }
 };
+
+class Solution
+{
+public:
+    bool Check(int Number, int Base)
+    {
+        string representation;
+
+        while (Number > 0)
+        {
+            representation += to_string(Number % Base);
+            Number /= Base;
+        }
+
+        int i = 0;
+        int j = representation.size() - 1;
+
+        cout << "Number: " << Number << " -> Base -> " << Base << " -> " << representation << endl;
+
+        while (i < j)
+        {
+            if (representation[i] != representation[j])
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    bool isStrictlyPalindromic(int n)
+    {
+        for (int i = 2; i <= n - 2; i++)
+            if (!Check(n, i))
+                return false;
+
+        return true;
+    }
+};
