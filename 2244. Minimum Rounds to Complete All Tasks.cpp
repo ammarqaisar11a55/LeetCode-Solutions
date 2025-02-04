d885a304b82c131c7098a8318a7f9a01fbca6a54
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-04 17:47:23 +0500

    Accepted Solution

---
+++ 2244. Minimum Rounds to Complete All Tasks.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+public:
+    int minimumRounds(vector<int> &tasks)
+    {
+        unordered_map<int, int> mp;
+
+        for (int c : tasks)
+            mp[c]++;
+
+        int operations = 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (auto x : mp)
+            pq.push(x.second);
+
+        while (!pq.empty())
+        {
+            int Top = pq.top();
+            pq.pop();
+
+            if (Top == 1)
+                return -1;
+
+            if (Top % 3 == 0)
+            {
+                operations += 1;
+                Top -= 3;
+            }
+            else if (Top % 2 == 0)
+            {
+                operations += 1;
+                Top -= 2;
+            }
+            else
+            {
+                operations += 1;
+                Top -= 3;
+            }
+
+            if (Top > 0)
+            {
+                pq.push(Top);
+            }
+        }
+
+        return operations;
+    }
+};
