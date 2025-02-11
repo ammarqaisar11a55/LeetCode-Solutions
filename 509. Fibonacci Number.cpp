198212d7be6cea2c051d4900102b1d30d579c388
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-11 20:45:49 +0500

    4 Approaches

--- 509. Fibonacci Number.cpp
+++ 509. Fibonacci Number.cpp
@@ -65,3 +65,31 @@ public:
         return DP[n];
     }
 };
+
+/*Constant Space*/
+
+class Solution
+{
+public:
+    int fib(int n)
+    {
+
+        if (n <= 1)
+            return n;
+
+        int prev2 = 0;
+        int prev1 = 1;
+
+        int res = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            res = prev2 + prev1;
+
+            prev2 = prev1;
+            prev1 = res;
+        }
+
+        return res;
+    }
+};
