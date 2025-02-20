a9b096bcf62af0face8ff0c23b8c6006f0bcab25
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-20 19:20:13 +0500

    2 Approaches 1st Accepted 2nd Passed 992/999 test cases

--- 3011. Find if Array Can Be Sorted.cpp
+++ 3011. Find if Array Can Be Sorted.cpp
@@ -18,3 +18,45 @@ public:
         return is_sorted(nums.begin(), nums.end());
     }
 };
+
+/*992/999 Passed: */
+
+bool canSortArray(vector<int> nums)
+{
+    unordered_map<int, queue<int>> mp;
+
+    for (int i : nums)
+    {
+        mp[__builtin_popcount(i)].push(i);
+    }
+
+    for (auto &group : mp)
+    {
+        vector<int> temp;
+        while (!group.second.empty())
+        {
+            temp.push_back(group.second.front());
+            group.second.pop();
+        }
+
+        sort(temp.begin(), temp.end());
+
+        for (int num : temp)
+        {
+            group.second.push(num);
+        }
+    }
+
+    int index = 0;
+    vector<int> Sorted;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int Bits = __popcount(nums[i]);
+
+        Sorted.push_back(mp[Bits].front());
+        mp[Bits].pop();
+    }
+
+    return is_sorted(Sorted.begin(), Sorted.end());
+}
