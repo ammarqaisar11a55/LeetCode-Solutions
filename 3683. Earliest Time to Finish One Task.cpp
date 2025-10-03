537962159cf37a9fe0579e5e1ec27938cbe15264
Muhammad Ammar Qaisar <164687897+ammarqaisar11a55@users.noreply.github.com>
2025-10-03 13:59:21 +0500

    Accepted Solution

---
+++ 3683. Earliest Time to Finish One Task.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+
+        int res = INT_MAX;
+
+        for(vector<int>task : tasks)
+        {
+            res = min(res,task[0]+task[1]);
+        }
+
+
+        return res;
+        
+    }
+};
