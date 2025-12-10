8a9e1f0a199c39536750c0f589445e0fd679eedc
Muhammad Ammar Qaisar <164687897+ammarqaisar11a55@users.noreply.github.com>
2025-12-10 21:26:54 +0500

    Accepted Solution

---
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void DFS(Node*& root, int current_level,unordered_map<int, stack<Node*>> &mp) {
+        if (root == NULL) {
+            return;
+        }
+
+        if(mp[current_level].empty())
+        {
+            root->next = NULL;
+            mp[current_level].push(root);
+        }
+        else
+        {
+            root->next = mp[current_level].top();
+            mp[current_level].push(root);
+        }
+
+        DFS(root->right,current_level + 1,mp);
+        DFS(root->left,current_level + 1,mp);
+
+    }
+
+    Node* connect(Node* root) {
+
+        unordered_map<int, stack<Node*>> level_Nodes;
+        DFS(root,0,level_Nodes);
+
+        return root;
+    }
+};
