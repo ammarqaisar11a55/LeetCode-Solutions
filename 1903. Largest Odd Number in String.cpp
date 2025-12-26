09aac6680a50a9e45bf5c555ad07afbc264b8d3e
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-12-26 19:53:30 +0500

    2 Approaches

--- 1903. Largest Odd Number in String.cpp
+++ 1903. Largest Odd Number in String.cpp
@@ -25,3 +25,24 @@ public:
         return res;
     }
 };
+
+/*Approach 2*/
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int index = -1;
+
+    for (int i = num.length() - 1; i >= 0; i--)
+    {
+        int digit = num[i] - '0';
+
+        if (digit & 1)
+        {
+            return num.substr(0, i + 1);
+        }
+    }
+
+    return "";
+    }
+};
