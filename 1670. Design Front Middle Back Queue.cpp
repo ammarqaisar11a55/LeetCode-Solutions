badc40d3349b8101009b1fb75d16ce8c35bba5c0
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2026-01-03 23:04:03 +0500

    1 Approach

--- 1670. Design Front Middle Back Queue.cpp
+++ 1670. Design Front Middle Back Queue.cpp
@@ -1,30 +1,35 @@
-class listNode {
+class listNode
+{
 public:
     int val;
-    listNode* next;
-    listNode* prev;
+    listNode *next;
+    listNode *prev;
 
-    listNode(int val) {
+    listNode(int val)
+    {
         this->val = val;
         this->prev = NULL;
         this->next = NULL;
     }
 };
 
-class FrontMiddleBackQueue {
+class FrontMiddleBackQueue
+{
 public:
 public:
-    listNode* head = nullptr;
-    listNode* tail = nullptr;
+    listNode *head = nullptr;
+    listNode *tail = nullptr;
     int current_size;
 
     FrontMiddleBackQueue() { current_size = 0; }
 
-    void pushFront(int val) {
-        listNode* node_to_insert = new listNode(val);
+    void pushFront(int val)
+    {
+        listNode *node_to_insert = new listNode(val);
         current_size++;
 
-        if (head == nullptr) {
+        if (head == nullptr)
+        {
             head = tail = node_to_insert;
 
             return;
@@ -35,36 +40,36 @@ public:
         head = node_to_insert;
     }
 
-    void pushMiddle(int val) {
-        listNode* node_to_insert = new listNode(val);
+    void pushMiddle(int val)
+    {
+        listNode *node_to_insert = new listNode(val);
 
-        if (head == nullptr) {
+        if (head == nullptr)
+        {
             current_size++;
             head = tail = node_to_insert;
 
             return;
         }
 
-        int position_to_reach;
+        int position_to_reach = (current_size - 1) / 2;
 
-        if (current_size % 2 == 0) {
-            position_to_reach = current_size / 2;
-        } else {
-            position_to_reach = (current_size + 1) / 2;
-        }
+        listNode *cralwer = head;
 
-        listNode* cralwer = head;
-
-        for (int i = 1; i < position_to_reach; i++) {
+        for (int i = 0; i < position_to_reach; i++)
+        {
             cralwer = cralwer->next;
         }
 
         node_to_insert->prev = cralwer;
         node_to_insert->next = cralwer->next;
 
-        if (cralwer->next != nullptr) {
+        if (cralwer->next != nullptr)
+        {
             node_to_insert->next->prev = node_to_insert;
-        } else {
+        }
+        else
+        {
             tail = node_to_insert;
         }
 
@@ -72,11 +77,13 @@ public:
         current_size++;
     }
 
-    void pushBack(int val) {
-        listNode* node_to_insert = new listNode(val);
+    void pushBack(int val)
+    {
+        listNode *node_to_insert = new listNode(val);
         current_size++;
 
-        if (tail == nullptr) {
+        if (tail == nullptr)
+        {
             head = tail = node_to_insert;
             return;
         }
@@ -86,12 +93,15 @@ public:
         tail = node_to_insert;
     }
 
-    int popFront() {
-        if (current_size == 0) {
+    int popFront()
+    {
+        if (current_size == 0)
+        {
             return -1;
         }
 
-        if (current_size == 1) {
+        if (current_size == 1)
+        {
             current_size--;
             int value_to_return = head->val;
             head = tail = nullptr;
@@ -100,7 +110,7 @@ public:
 
         current_size--;
 
-        listNode* node_to_delete = head;
+        listNode *node_to_delete = head;
         head = head->next;
 
         head->prev = nullptr;
@@ -111,39 +121,38 @@ public:
         return value_to_return;
     }
 
-    int popMiddle() {
-        if (current_size == 0) {
+    int popMiddle()
+    {
+        if (current_size == 0)
+        {
             return -1;
         }
 
-        if (current_size == 1) {
+        if (current_size == 1)
+        {
             current_size--;
             int value_to_return = head->val;
             head = tail = nullptr;
             return value_to_return;
         }
 
-        int position_to_reach;
+        int position_to_reach = (current_size - 1) / 2;
 
-        if (current_size % 2 == 0) {
-            position_to_reach = current_size / 2;
-        } else {
-            position_to_reach = (current_size + 1) / 2;
-        }
+        listNode *cralwer = head;
 
-        listNode* cralwer = head;
-
-        for (int i = 1; i < position_to_reach; i++) {
+        for (int i = 0; i < position_to_reach; i++)
+        {
             cralwer = cralwer->next;
         }
 
-        listNode* node_to_delete = cralwer->next;
+        listNode *node_to_delete = cralwer->next;
 
-        if (node_to_delete == head) {
-            delete node_to_delete;
+        if (node_to_delete == head)
+        {
             return popFront();
-        } else if (node_to_delete == tail) {
-            delete node_to_delete;
+        }
+        else if (node_to_delete == tail)
+        {
             return popBack();
         }
 
@@ -160,12 +169,15 @@ public:
         return value_to_return;
     }
 
-    int popBack() {
-        if (current_size == 0) {
+    int popBack()
+    {
+        if (current_size == 0)
+        {
             return -1;
         }
 
-        if (current_size == 1) {
+        if (current_size == 1)
+        {
             current_size--;
             int value_to_return = tail->val;
             head = tail = nullptr;
@@ -174,7 +186,7 @@ public:
 
         current_size--;
 
-        listNode* node_to_delete = tail;
+        listNode *node_to_delete = tail;
         tail = tail->prev;
         tail->next = nullptr;
         node_to_delete->prev = nullptr;
@@ -196,4 +208,4 @@ public:
  * int param_6 = obj->popBack();
  */
 
- /*Unable to locate the error of above approach*/
+/*Unable to locate the error of above approach*/
